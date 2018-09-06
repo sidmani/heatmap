4656cfa7c80958c57561160dfccbe83a9520ec26
Sid Mani <sid@sidmani.com>
2018-09-06 14:19:19 -0700

    fix issue with undefined element

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const char = '▦ ';
 module.exports = function heatmap(data, colors, rows = data.length, columns = data[0].length) {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < columns; j++) {
-      process.stdout.write(chalk.hex(colors[data[i][j]])(char));
+      const hex = data[i][j] && colors[data[i][j]] ? colors[data[i][j]] : '000000';
+      process.stdout.write(chalk.hex(hex)(char));
     }
     process.stdout.write('\n');
   }
