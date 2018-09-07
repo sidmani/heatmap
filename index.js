a94342c14420e7ff9a1183f5e63149c1d49d8d48
Sid Mani <sid@sidmani.com>
2018-09-06 17:42:42 -0700

    color interpolation

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const chalk = require('chalk');
 const char = '▦ ';
-module.exports = function heatmap(data, colors, rows = data.length, columns = data[0].length) {
+
+function map(val, inMin, inMax, rgbMin, rgbMax) {
+  const frac = (val - inMin) / (inMax - inMin);
+  return rgbMin.map((v, idx) => Math.min(Math.max((rgbMax[idx] - v) * frac + v, rgbMax[idx]), v));
+}
+
+function hexToRGB(hex) {
+  return /#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})/.exec(hex).slice(1).map(v => parseInt(v, 16));;
+}
+
+module.exports = function heatmap(data, minColor, maxColor, minValue, maxValue) {
+  const rows = data.length;
+  const columns = data[0].length;
+
+  const minColorRGB = hexToRGB(minColor);
+  const maxColorRGB = hexToRGB(maxColor);
+
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < columns; j++) {
-      const hex = data[i][j] && colors[data[i][j]] ? colors[data[i][j]] : '000000';
+      const value = data[i][j] || 0;
+      const hex = map(value, minValue, maxValue, minColorRGB, maxColorRGB).map(x => x.toString(16)).join('');
       process.stdout.write(chalk.hex(hex)(char));
     }
     process.stdout.write('\n');
